1376cdbbecf7b5fcd287214da78638e1249c14b5
Neirene <myriabelmonte@gmail.com>
2018-02-06 17:18:19 +0100

    Small Annotation Change

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <transition appear name="v--mask">
     <footer v-once class="blog__footer">
-      Licensed by MOGURA, Copyright Nei Softworks 2000-2018 footer component
+      WORK IN PROGRESS - Licensed by MOGURA, Copyright Nei Softworks 2000-2018
     </footer>
   </transition>
 </template>
